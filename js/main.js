3629830c95cb03f28a0c84706e96a7a44b809384
xep2cm <whitebeatstore@gmail.com>
2025-06-15 01:21:39 +0500

    4

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
-window.onload = function() {
-    // Функция для обработки изменения состояния чекбокса отдельной задачи
-    document.querySelectorAll('.lesson__list input').forEach(input => {
-        input.addEventListener('change', function() {
-            const task = this.nextElementSibling; // Следующий элемент (текст)
-            if (this.checked) {
-                task.classList.add('completed'); // Добавляем класс 'completed' при выборе
-            } else {
-                task.classList.remove('completed'); // Удаляем класс 'completed'
-            }
-        });
+// Обработка изменения состояния чекбокса отдельной задачи
+document.querySelectorAll(".lesson__list input").forEach(input => {
+    input.addEventListener("change", function () {
+        const task = this.nextElementSibling; // Следующий элемент (текст)
+        if (this.checked) {
+            task.classList.add("completed"); // Добавляем класс 'completed' при выборе
+        } else {
+            task.classList.remove("completed"); // Удаляем класс 'completed'
+        }
     });
+});
 
-    // Функция для обработки изменения состояния чекбокса целого раздела
-    document.querySelectorAll('.section-toggle').forEach(toggle => {
-        toggle.addEventListener('change', function() {
-            const sectionContent = this.closest('.section-label').nextElementSibling; // Получаем ближайший родительский блок (.section-label), затем получаем следующего брата
-            if (this.checked) {
-                sectionContent.classList.add('completed');
-            } else {
-                sectionContent.classList.remove('completed');
-            }
-        });
+// Обработка изменения состояния чекбокса целого раздела
+document.querySelectorAll(".section-toggle").forEach(toggle => {
+    toggle.addEventListener("change", function () {
+        const sectionContent = this.closest(".section-label").nextElementSibling; // Получаем ближайший родительский блок (.section-label), затем получаем следующего брата
+        if (this.checked) {
+            sectionContent.classList.add("completed");
+        } else {
+            sectionContent.classList.remove("completed");
+        }
     });
-};
+});
